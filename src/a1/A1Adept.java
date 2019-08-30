7d18116bf7108262f199e18e6bc6bece15396d2b
wyawyatt <wyawyatt@wyawyatt-pc>
2019-08-30 17:31:00 -0400

    Commented out the printed Java version number

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -3,12 +3,118 @@ package a1;
 import java.util.Scanner;
 
 public class A1Adept {
+	static String firstVar = "";
+	static String lastVar = "";
+	static String firstSmall = "";
+	static String lastSmall = "";
 
 	public static void main(String[] args) {
 		
+		System.out.println("Your Java Version: " + System.getProperty("java.version"));
+		
 		Scanner scan = new Scanner(System.in);
+		
+		int itemCount = scan.nextInt();
+		double[] prices = new double[itemCount];
+		String[] items = new String[itemCount];
 
-		// Your code follows here.
+				
+		for (int i=0; i<itemCount; i++) {
+				items[i] = scan.next();
+				prices[i] = scan.nextDouble();
+				
+				}
+		int customerCount = scan.nextInt();
+		int[] customers = new int[customerCount];
+		double[] totalArray = new double[customerCount];
+		String[] first = new String[customerCount];
+		String[] last = new String[customerCount];
+		
+		
+		
+		for (int i=0; i<customerCount; i++) {
+			first[i] = scan.next();
+		    last[i] = scan.next();
+			int itemsBought = scan.nextInt();
+			double total = 0.00;
+			
+			for (int j=0; j<itemsBought; j++) {
+				int quant = scan.nextInt();
+				String name = scan.next();
+				double price = 0.00;
+				
+				for (int k=0; k<items.length; k++) {
+					if (items[k].equals(name) ) {
+						price = prices[k];
+						
+					}
+					
+			}
+				total = total + price * quant;
+				totalArray[i] = total;
+				
+				
+				
+				}
+			
+			
+		}
+		
+		double big = getBiggest(totalArray, first, last );
+		double small = getSmallest(totalArray, first, last);
+		double average = getAverage(totalArray, customerCount);
+		
+	
+		
+		
+		
+		System.out.println("Biggest: " + firstVar + " " + lastVar + " " + "(" + String.format("%.2f", big) + ")");
+		System.out.println("Smallest: " + firstSmall + " " + lastSmall + " " + "(" + String.format("%.2f", small) + ")");
+		System.out.println("Average: " + String.format("%.2f", average));
+		
+			
+
+		
+		
+	}
+	
+	static double getBiggest(double[] total, String[] first, String[] last) {
+		double value = total[0];
+		for(int i=1; i<total.length; i++) {
+			if (total[i] > value) {
+				value = total[i];
+				firstVar = first[i];
+				lastVar = last[i];
+				
+				
+			}
+		}
+		return value;
+		
+	}
+	
+	static double getSmallest(double[] total, String[] first, String[] last) {
+		double value = total[0];
+		for (int i=0; i<total.length; i++ ) {
+			if (total[i] < value) {
+				value = total[i];
+				firstSmall = first[i];
+				lastSmall = last[i];
+			}
+		}
+		return value;
+	}
+	
+	static double getAverage(double[] total, int customers) {
+		double value = 0.00;
+		for (int i=0; i<total.length; i++) {
+			value = value + total[i];
+		}
+		value = value/customers;
+		return value;
+				
 		
 	}
 }
+
+
--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -6,9 +6,40 @@ public class A1Novice {
 
 	public static void main(String[] args) {
 		
-		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
+		System.out.println("Your Java version: " + System.getProperty("java.version"));
 		
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int count = scan.nextInt();
+		int[] values = new int[count];
+		double total = 0;
+		
+		
+		
+		for (int i=0; i<values.length; i++) {
+		 String first = scan.next();
+		 String last = scan.next();
+		 values[i] = scan.nextInt();
+		 
+		 
+		 for (int j=0; j<values[i]; j++) {
+			 int quant = scan.nextInt();
+			 String name = scan.next();
+			 double price = scan.nextDouble();
+			 total = total + quant * price;
+		
+		 
+		}
+		 System.out.println(first.charAt(0) + "." + " " + last + ": " +  String.format("%.2f", total));
+		 total = 0;
+			
+			
+		}
+		scan.close();
+		
+		 }
 	}
-}
+
+
+
