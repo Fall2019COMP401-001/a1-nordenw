7ecc7cd9fa059715e1a4660ef7efdc2cd0d38fd1
wyawyatt <wyawyatt@wyawyatt-pc>
2019-09-03 15:56:26 -0400

    first time submitting a1jedi

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -7,8 +7,55 @@ public class A1Jedi {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
 		
+		int itemCount = scan.nextInt();
+		String items[] = new String[itemCount];
+		int amountBought[] = new int[itemCount];
+		
+		for (int i=0; i<itemCount; i++) {
+			items[i] = scan.next();
+			double price = scan.nextDouble();
+		}
+		
+		int customerCount = scan.nextInt();
+		int customerNumber[] = new int[itemCount];
+		int total[] = new int[itemCount];
+		//String lastName[] = new String[customerCount];
+		int itemsBought[] = new int[customerCount];
+		int quantArray[] = new int[itemCount];
+		String itemName[] = new String[itemCount];
+		
+		
+		for (int j=0; j<customerCount; j++) {
+			String first = scan.next();
+			String last = scan.next();
+		    itemsBought[j] = scan.nextInt();
+			
+			for (int k=0; k<itemsBought[j]; k++) {
+				quantArray[k] = scan.nextInt();
+				itemName[k] = scan.next();
+				
+				for (int l=0; l<itemCount; l++) {
+					if (itemName[k].equals(items[l])) {
+						customerNumber[l]++;
+						total[l] += quantArray[k];
+					}
+				}
+				
+			}
+			}
+		
+		
+		for (int i=0; i<itemCount; i++) {
+		if (customerNumber[i] == 0) {
+			System.out.println("No customers bought" + " " + items[i]);
+		} else {
+			System.out.println(customerNumber[i] + " " + "customers bought " + total[i] + " " + items[i]);
+		}
+}
 	}
 }
+		
+		
+		
+		 
